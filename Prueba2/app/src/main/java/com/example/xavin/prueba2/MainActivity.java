4c07c1b07d5e2af191c4706f5d366ff738e6971f
Xavi Navarro <xavinavarro@users.noreply.github.com>
2017-09-26 12:15:48 +0200

    Layout

--- Prueba2/app/src/main/java/com/example/xavin/prueba2/MainActivity.java
+++ Prueba2/app/src/main/java/com/example/xavin/prueba2/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.xavin.prueba2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -15,10 +17,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // final TextView mensajeIni;
+
         final EditText miTexto= (EditText)findViewById(R.id.miTxt);
         final Button miBoton= (Button)findViewById(R.id.miBtn);
         final TextView elSaludo= (TextView)findViewById(R.id.miLbl);
+
 
         miTexto.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             public void onFocusChange(View v, boolean b){
@@ -37,13 +40,15 @@ public class MainActivity extends Activity {
                 miBundle.putString("TEXTO", mensajePaso);
                 miIntent.putExtras(miBundle);
 
-
                 startActivity(miIntent);
             }
 
         });
-
-
     }
-
+    protected void  showToast(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
--- Prueba2/app/src/main/java/com/example/xavin/prueba2/Pantalla2.java
+++ Prueba2/app/src/main/java/com/example/xavin/prueba2/Pantalla2.java
@@ -4,20 +4,33 @@ package com.example.xavin.prueba2;
  * Created by xavin on 19/09/2017.
  */
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Pantalla2 extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
 
-        final TextView otroSaludo= (TextView)findViewById(R.id.miMensaje);
+        final TextView otroSaludo = (TextView) findViewById(R.id.miMensaje);
+
+        showToast("Estoy en la pantalla2");
 
         Bundle  miBundleRecoger = getIntent().getExtras();
         otroSaludo.setText(miBundleRecoger.getString("TEXTO"));
+
+    }
+
+    protected void showToast(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
 
     }
 }
