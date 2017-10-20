03eccdb3ee1e8620e17e6b37318e3ade66cab703
Xavi Navarro <xavinavarro@users.noreply.github.com>
2017-10-20 11:59:25 +0200

    Update

---
+++ SpinnerYListaSimple/app/src/main/java/com/example/xavin/spinnerylistasimple/SpinnerPersona.java
@@ -0,0 +1,17 @@
+package com.example.xavin.spinnerylistasimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpinnerPersona extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_persona);
+
+
+    }
+}
