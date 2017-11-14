8b36f5719c395dcff523d13f45d8999b4da27236
Xavi Navarro <xavinavarro@users.noreply.github.com>
2017-11-14 10:32:20 +0100

    Update

--- EjercicioRecopilatorio/app/src/main/java/com/example/xavin/ejerciciorecopilatorio/MainActivity.java
+++ EjercicioRecopilatorio/app/src/main/java/com/example/xavin/ejerciciorecopilatorio/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.xavin.ejerciciorecopilatorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerObjeto;
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
     }
