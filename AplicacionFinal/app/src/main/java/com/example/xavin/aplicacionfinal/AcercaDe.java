c753becd784d1cc45de68ad84b40e7eaf345da60
Xavi Navarro <xavinavarro@users.noreply.github.com>
2018-02-08 19:14:45 +0100

    Añadido AcercaDe

--- AplicacionFinal/app/src/main/java/com/example/xavin/aplicacionfinal/AcercaDe.java
+++ AplicacionFinal/app/src/main/java/com/example/xavin/aplicacionfinal/AcercaDe.java
@@ -1,15 +1,15 @@
 package com.example.xavin.aplicacionfinal;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.view.View;
+    import android.widget.Button;
 
 public class AcercaDe extends AppCompatActivity {
 
 
-    protected void onCrete(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
 
@@ -18,8 +18,8 @@ public class AcercaDe extends AppCompatActivity {
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent volver = new Intent(AcercaDe.this, MainActivity.class);
-                startActivity(volver);
+                Intent irvolver = new Intent(AcercaDe.this, MainActivity.class);
+                startActivity(irvolver);
             }
         });
     }
